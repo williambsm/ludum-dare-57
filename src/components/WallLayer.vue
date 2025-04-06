49fcf4449ec779dddcc96e25a85383c80f6ec03b
William Ste-Marie <williambsm@hotmail.fr>
2025-04-05 21:27:07 -0400

    Fix formatting to two spaces

--- src/components/WallLayer.vue
+++ src/components/WallLayer.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="wall-layer mapObject" :style="{ height: height + 'px',  bottom: bottom + 'px'}">
-        <wall class="left"/>
-        {{ wall.id }} - {{  hasIntersected }} - {{  isIntersecting }} - {{ bottom }}
-        <wall class="right"/>
-    </div>
+  <div class="wall-layer mapObject" :style="{ height: height + 'px',  bottom: bottom + 'px'}">
+    <wall class="left"/>
+    {{ wall.id }} - {{  hasIntersected }} - {{  isIntersecting }} - {{ bottom }}
+    <wall class="right"/>
+  </div>
 </template>
 
 <style>
- .wall-layer{
+  .wall-layer {
     position: absolute !important;
     width:100%;
     background-color: rgba(0,0,0,0.1);
     display:flex;
     justify-content: space-between;
     flex-shrink: 0;
- }
+  }
 </style>
 
 <script>
@@ -22,54 +22,53 @@ import Wall from "@/components/Wall.vue";
 import { mapGetters } from 'vuex';
 import { mapActions } from 'vuex';
 
-
 export default {
     name: 'WallLayer',
     props: ['wall'],
     emits: ['outside'],
     components: {
-        Wall
+      Wall
     },
     data() {
-        return {
-            height: 10,
-            bottom: 0,
-            hasIntersected: false,
-            isIntersecting: false,
-        }
+      return {
+        height: 10,
+        bottom: 0,
+        hasIntersected: false,
+        isIntersecting: false,
+      }
     },
     computed: {
-        ...mapGetters(['roundDepth']),
+      ...mapGetters(['roundDepth']),
     },
     methods: {
-        ...mapActions(['addWall', 'removeWall']),
+      ...mapActions(['addWall', 'removeWall']),
     },
     mounted(){
-        this.height = this.wall.height;
-        this.bottom = this.wall.bottom;
+      this.height = this.wall.height;
+      this.bottom = this.wall.bottom;
 
-        const mapScreen = document.querySelector('.world');
+      const mapScreen = document.querySelector('.world');
 
-        // Create an intersection observer
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        this.hasIntersected = true;
-                        this.isIntersecting = true;
-                    } else {
-                        this.isIntersecting = false;
-                        if (this.hasIntersected) {
-                            this.removeWall();
-                        }
-                    }
-            });
-            }, {
-            // Options for the observer: we want to detect any intersection
-                root: mapScreen, // Use the viewport as the root
-                rootMargin: '0px',
-                threshold: 0 // 10% of the target element needs to be visible
-            });                
-        observer.observe(this.$el);
-    }
+      // Create an intersection observer
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.hasIntersected = true;
+            this.isIntersecting = true;
+          } else {
+            this.isIntersecting = false;
+            if (this.hasIntersected) {
+              this.removeWall();
+            }
+          }
+        });
+      }, {
+        root: mapScreen,
+        rootMargin: '0px',
+        threshold: 0,
+      });
+
+      observer.observe(this.$el);
+  }
 }
 </script>
