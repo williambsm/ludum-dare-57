1df822cd2404b76a3dde0573ce0b1ba0c8518349
Aaron Atkinson <38330431+AaronAtkinson@users.noreply.github.com>
2025-04-05 20:20:37 -0400

    Walls

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,9 @@ export default createStore({
             difficulty: 'easy',
             time: null,
             score: 0,
-            depth: 0
+            depth: 0,
+            wallHeight: 100,
+            map:[]
         },
 
     },
@@ -45,6 +47,28 @@ export default createStore({
         },
         setRoundDepth(state, value) {
             state.round.time = value;
+            state.round.depth = value;
+        },
+        addWall(state, value) {
+            state.round.map.push(value);
+        },
+        removeWall(state) {
+            console.log("removing wall more");
+            state.round.map.slice(1);
+            // state.round.map.slice(0,1);
+            // Vue.delete(state.round.map, 0);
+            // state.items = state.items.slice(1);
+            // state.round.map.slice(1);
+            // console.log(state.round.map);
+            // state.round.map = state.round.map.slice(1);
+            // console.log(state.round.map);
+            // state.round.map = state.round.map.slice(1);
+
+            
+            // Splice removes the first item of the array
+
+            // This line ensures Vue reacts to the change if using Vue 2.x
+            // state.round.map = [...state.round.map.splice(1)]; // Creates a new array reference
         },
     },
     actions: {
@@ -56,6 +80,32 @@ export default createStore({
         },
         updateDepth(context, value) {
             context.commit("setRoundDepth", value);
+        },
+
+        removeWall(context){
+            console.log("removing wall");
+            context.commit("removeWall");
+            context.dispatch("addWall");
+        },
+        addWall(context){
+            // might add some logic here to change 'biomes' at certain depths, etc. this might change the neww wall's height
+            
+            const newWallHeight = context.getters.wallHeight;
+
+            var lastWall = context.getters.wallCount == 0 
+            ? { id : 0, bottom: 0, height: 0 } 
+            : context.getters.lastWall;
+
+            console.log("lastWall");
+            console.log(lastWall);
+            const newWall = { 
+                id : lastWall.id + 1, 
+                height: newWallHeight, 
+                // bottom: lastWall.height + lastWall.bottom,
+                bottom: newWallHeight * (lastWall.id),
+                biome: "beach",
+            };
+            context.commit("addWall", newWall);
         },
     },
     modules: {}
