183ed3c45653c2188a903d74f37ec1c0079ee03f
Aaron Atkinson <38330431+AaronAtkinson@users.noreply.github.com>
2025-04-06 13:10:46 -0400

    Clean title view
    Cleaned up title screen

--- src/views/TitleView.vue
+++ src/views/TitleView.vue
@@ -7,16 +7,9 @@
 
 
 <script>
-// MIXINS
-// import { StoreMixin } from "@/mixins/StoreMixin.js";
-
-// COMPONENTS
 import Button from "@/components/ui/Button.vue";
 
-// VUEX
-// import { mapState } from "vuex";
-import { mapGetters } from "vuex";
-import { mapActions } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "TitleView",
